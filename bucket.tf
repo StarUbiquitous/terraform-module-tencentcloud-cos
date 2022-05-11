6cce97505eb84aa2983d655c658a5691b46a76fa
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2022-05-11 16:17:56 +0800

    feat: update var and bucket
    Signed-off-by: Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>

---
+++ bucket.tf
@@ -0,0 +1,38 @@
+locals {
+  cors_rules      = try(jsondecode(var.cors_rules), var.cors_rules)
+  lifecycle_rules = try(jsondecode(var.lifecycle_rules), var.lifecycle_rules)
+}
+
+resource "tencentcloud_cos_bucket" "bucket" {
+  bucket = var.bucket
+  acl    = var.acl
+  multi_az = var.multi_az
+  versioning_enable = var.versioning_enable
+
+  dynamic "cors_rules" {
+    for_each = local.cors_rules.allowed_headers
+    content {
+      allowed_origins = try(local.cors_rules.allowed_origins, null)
+      allowed_methods = try(local.cors_rules.allowed_methods, null)
+      allowed_headers = try(local.cors_rules.allowed_headers, null)
+      expose_headers  = try(local.cors_rules.expose_headers, null)
+      max_age_seconds = try(local.cors_rules.max_age_seconds, null)
+    }
+  }
+
+  dynamic "lifecycle_rules" {
+    for_each = local.lifecycle_rules
+    content {
+      filter_prefix = try(local.lifecycle_rules.filter_prefix, null)
+
+      transition {
+        date          = try(local.lifecycle_rules.transition.date, null)
+        storage_class = try(local.lifecycle_rules.transition.storage_class, null)
+      }
+
+      expiration {
+        days = try(local.lifecycle_rules.expiration.days, null)
+      }
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,12 +1,31 @@
-variable "region" {
-  default     = "ap-chengdu"
-  description = "The region to deploy in, e.g: ap-shanghai"
+variable "bucket" {
+  type = string
+}
+
+variable "acl" {
+  default     = "private"
+  description = "The ACL to use for the bucket"
   type        = string
 }
 
-locals {
-  iata = {
-    ap-shanghai = "SHA"
-    ap-chengdu  = "CTU"
-  }
+variable "multi_az" {
+  default     = false
+  description = "Whether to enable multi-az"
+  type        = bool
+}
+
+variable "versioning_enable" {
+  default     = false
+  description = "Enable versioning on the bucket"
+  type        = bool
+}
+
+variable "cors_rules" {
+  default     = {}
+  description = "The CORS rules to apply to the bucket"
+}
+
+variable "lifecycle_rules" {
+  default     = {}
+  description = "The lifecycle rules to apply to the bucket"
 }
